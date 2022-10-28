e259b4082a9f9175d7e21c6a8d638da98fe685a5
Tiago Viana <tiagoviana.1003@gmail.com>
2022-10-29 00:47:17 +0100

    - Update start the etree

--- src/rpc-client/main.py
+++ src/rpc-client/main.py
@@ -1,17 +1,27 @@
 import xmlrpc.client
 import pandas as pd
+import xml.etree.cElementTree as ET
 
 print("connecting to server...")
 server = xmlrpc.client.ServerProxy('http://localhost:9000')
 
 string = "hello world"
 
-
 def readDataset():
     dataset = pd.read_csv("../master.csv")
     print(dataset)
     return dataset
+def writeToXML(dataset:pd):
+    root = ET.Element("Suicides")
+    doc = ET.SubElement(root, "doc")
+    for ind in dataset.index:
+        print(dataset['country'][ind])
 
 print(f" > {server.string_reverse(string)}")
 print(f" > {server.string_length(string)}")
-readDataset()
+#dataset = readDataset()
+
+#print(len(dataset))
+#print(dataset.iterrows())
+writeToXML(readDataset())
+
